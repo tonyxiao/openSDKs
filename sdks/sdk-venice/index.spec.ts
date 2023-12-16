60a314f2cd375c3c4c5341478f1ca2c149db785c
Tony Xiao <tonyx.ca@gmail.com>
2023-12-16 02:43:27 -0100

    fix: Increase test timeout to be more resilient

--- sdks/sdk-venice/index.spec.ts
+++ sdks/sdk-venice/index.spec.ts
@@ -1,5 +1,8 @@
+import {jest} from '@jest/globals'
 import {initSDK} from '@opensdks/runtime'
 import {veniceSdkDef} from './index.js'
+
+jest.setTimeout(70 * 15 * 1000) // In case of cold start
 
 const venice = initSDK(veniceSdkDef, {headers: {}})
 
