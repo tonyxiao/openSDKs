35c655ab718b5d6969324930e21e74032fdc2fe1
Tony Xiao <tonyx.ca@gmail.com>
2023-12-26 11:52:00 -0100

    feat: Add method to modifyUrl

--- packages/fetch-links/modifyRequestResponse.spec.ts
+++ packages/fetch-links/modifyRequestResponse.spec.ts
@@ -1,12 +1,45 @@
-import {modifyRequest} from './modifyRequestResponse.js'
+import {modifyRequest, modifyUrl} from './modifyRequestResponse.js'
+
+test('modifyUrl', () => {
+  expect(
+    modifyUrl('https://httpbin.org/anything', {protocol: 'http:'}),
+  ).toEqual('http://httpbin.org/anything')
+  expect(modifyUrl('https://httpbin.org/anything', {protocol: 'http'})).toEqual(
+    'http://httpbin.org/anything',
+  )
+  expect(modifyUrl('https://httpbin.org/anything', {port: '3000'})).toEqual(
+    'https://httpbin.org:3000/anything',
+  )
+})
+
+test('modifyUrl searchParams', () => {
+  expect(
+    modifyUrl('https://httpbin.org/anything', {searchParams: {a: 1}}),
+  ).toEqual('https://httpbin.org/anything?a=1')
+
+  expect(modifyUrl('/anything', {searchParams: {a: 1}})).toEqual(
+    '/anything?a=1',
+  )
+
+  expect(modifyUrl('/anything?va=11', {searchParams: {a: 1}})).toEqual(
+    '/anything?va=11&a=1',
+  )
+  expect(
+    modifyUrl('/anything?va=11', {searchParams: (sp) => sp.delete('va')}),
+  ).toEqual('/anything')
+})
 
 test('modifyRequest', () => {
   const req = new Request('https://httpbin.org/anything', {method: 'GET'})
 
   expect(req.body).toBeNull()
-  const req2 = modifyRequest(req, {method: 'HEAD'})
+  const req2 = modifyRequest(req, {
+    method: 'HEAD',
+    url: {searchParams: {hello: 'world'}},
+  })
   expect(req2.method).toEqual('HEAD')
   expect(req2.body).toBeNull()
+  expect(req2.url).toEqual('https://httpbin.org/anything?hello=world')
 })
 
 test('modifyRequest with body', () => {
--- packages/fetch-links/modifyRequestResponse.ts
+++ packages/fetch-links/modifyRequestResponse.ts
@@ -1,31 +1,92 @@
 import type {HTTPMethod} from './link.js'
 
+/**
+ * Conditionally get body to avoid issues with
+ * "TypeError: Response body object should not be disturbed or locked"
+ * "TypeError: Request with GET/HEAD method cannot have body."
+ * @see https://share.cleanshot.com/KPYHPC5n
+ * Typically used together with modifyRequest
+ */
+export async function maybeGetBody(req: Request) {
+  return ['POST', 'PUT', 'PATCH'].includes(req.method) ? await req.blob() : null
+}
+
+type URLOverrides = {
+  /** Either overrides or fn that does in-place modification */
+  searchParams?: Record<string, unknown> | ((sp: URLSearchParams) => void)
+} & Partial<
+  Pick<
+    URL,
+    | 'hash'
+    | 'hostname'
+    | 'href'
+    | 'password'
+    | 'pathname'
+    | 'port'
+    | 'protocol'
+    | 'username'
+  >
+>
+
+export function modifyUrl(
+  urlStr: string,
+  {searchParams, ...overrides}: URLOverrides,
+) {
+  // Workaround the fact that URL API is not able to parse partial relative URLs
+  const tempBase = urlStr.includes('//') ? undefined : 'http://temp'
+  const url = new URL(urlStr, tempBase)
+
+  for (const [key, value] of Object.entries(overrides ?? {})) {
+    url[key as keyof typeof overrides] = value
+  }
+  if (typeof searchParams === 'function') {
+    searchParams(url.searchParams)
+  } else {
+    for (const [key, value] of Object.entries(searchParams ?? {})) {
+      url.searchParams.set(key, String(value))
+    }
+  }
+  return url.toString().slice(tempBase?.length)
+}
+
 export function modifyRequest(
   req: Request,
   overrides: Omit<RequestInit, 'headers'> & {
     method?: HTTPMethod
-    url?: string
+    url?: string | URLOverrides
+    /** Either overrides or fn that does in-place modification */
     headers?: HeadersInit | ((headers: Headers) => void)
+    /**
+     * Needed when passing through body as ReadableStream of byteArray. Solves
+     * "TypeError: RequestInit: duplex option is required when sending a body."
+     * TODO: Think about if we should better abstract this somehow...
+     */
+    duplex?: 'half'
   },
 ) {
-  return new Request(overrides.url ?? req.url, {
-    // only include body if it exists
-    // otherwise we get TypeError: cannot include body for GET / HEAD requests
-    ...(req.body != null && {body: req.body}), 
-    // Can we spread this? Or is there a better way to duplicate
-    method: req.method,
-    cache: req.cache,
-    credentials: req.credentials,
-    integrity: req.integrity,
-    keepalive: req.keepalive,
-    mode: req.mode,
-    redirect: req.redirect,
-    referrer: req.referrer,
-    referrerPolicy: req.referrerPolicy,
-    signal: req.signal,
-    ...overrides,
-    headers: modifyHeaders(req.headers, overrides.headers),
-  })
+  return new Request(
+    typeof overrides.url === 'string'
+      ? overrides.url
+      : overrides.url
+        ? modifyUrl(req.url, overrides.url)
+        : req.url,
+    {
+      // Can we spread this? Or is there a better way to duplicate
+      method: req.method,
+      body: req.body,
+      cache: req.cache,
+      credentials: req.credentials,
+      integrity: req.integrity,
+      keepalive: req.keepalive,
+      mode: req.mode,
+      redirect: req.redirect,
+      referrer: req.referrer,
+      referrerPolicy: req.referrerPolicy,
+      signal: req.signal,
+      ...overrides,
+      headers: modifyHeaders(req.headers, overrides.headers),
+    },
+  )
 }
 
 export function modifyResponse(
