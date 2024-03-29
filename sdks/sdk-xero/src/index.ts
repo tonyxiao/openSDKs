88e4691585d8b4d02a46699f1d92490b1a4e0445
Jatin Sandilya <7681067+jatinsandilya@users.noreply.github.com>
2024-03-29 12:25:19 +0530

    feat: add xero sdk (#6)
    * feat: add sdk-xero with accounting api endpoint
    
    * fix: exclude examples directory from ts config

---
+++ sdks/sdk-xero/src/index.ts
@@ -0,0 +1,20 @@
+import type {ClientOptions, SdkDefinition, SDKTypes} from '@opensdks/runtime'
+import type xeroTypes from '../xero_accounting.oas.types.js'
+import {default as xeroOasMeta} from './xero_accounting.oas.meta.js'
+
+export type XeroSDKTypes = SDKTypes<
+  xeroTypes,
+  Omit<ClientOptions, 'headers'> & {
+    headers: {
+      authorization: `Bearer ${string}`
+      [k: string]: string
+    }
+  }
+>
+
+export const xeroSdkDef = {
+  types: {} as XeroSDKTypes,
+  oasMeta: xeroOasMeta,
+} satisfies SdkDefinition<XeroSDKTypes>
+
+export default xeroSdkDef
