06aaffa47e418a517923073ee6f7ac12042dbe5e
Tony Xiao <tonyx.ca@gmail.com>
2024-04-01 15:30:19 -0700

    fix(xero): test

--- sdks/sdk-xero/src/index.spec.ts
+++ sdks/sdk-xero/src/index.spec.ts
@@ -6,16 +6,18 @@ const accessToken = process.env['XERO_ACCESS_TOKEN']!
 const maybeTest = accessToken ? test : test.skip
 
 maybeTest('list accounts', async () => {
-  const xero = initXeroSDK({
+  const xero1 = initXeroSDK({
     headers: {authorization: `Bearer ${accessToken}`},
   })
-
-  const conns = await xero.identity.GET('/Connections')
+  const conns = await xero1.identity.GET('/Connections')
   const firstTenantId = conns.data[0]?.tenantId
-
-  const res = await xero.accounting.GET('/Accounts', {
-    params: {header: {'xero-tenant-id': firstTenantId!}},
+  const xero = initXeroSDK({
+    headers: {
+      authorization: `Bearer ${accessToken}`,
+      'xero-tenant-id': firstTenantId,
+    },
   })
+  const res = await xero.accounting.GET('/Accounts', {})
 
   expect(res.data.Accounts?.length).toBeGreaterThanOrEqual(0)
 })
