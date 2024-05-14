7c364c6f840d325963f2e8d5ba907048dc6ef3d0
jatin <sandilya.jatin@gmail.com>
2024-05-14 16:27:52 +0530

    feat: add offers & basic auth to lever sdk

--- sdks/sdk-lever/src/index.ts
+++ sdks/sdk-lever/src/index.ts
@@ -8,7 +8,19 @@ export {leverTypes}
 export type LeverSDKTypes = SDKTypes<
   leverTypes,
   Omit<ClientOptions, 'headers'> & {
-    headers: {
+    /**
+     * Lever API key becomes the username here with password always kept as
+     * an empty string when used internally with API key alone.
+     *
+     * When used for external use-case, oauth with bearer token is used.
+     */
+    auth?: {
+      basic: {
+        username: `${string}`
+        password?: ''
+      }
+    }
+    headers?: {
       authorization: `Bearer ${string}`
       [k: string]: string | undefined
     }
@@ -18,6 +30,23 @@ export type LeverSDKTypes = SDKTypes<
 export const leverSdkDef = {
   types: {} as LeverSDKTypes,
   oasMeta: leverOasMeta,
+  createClient(ctx, {...opts}) {
+    const headers = new Headers(opts['headers'] as HeadersInit)
+    if (
+      opts['auth'] &&
+      !(opts['headers'] && opts['headers']['authorization'])
+    ) {
+      headers.set(
+        'Authorization',
+        `Basic ${btoa(
+          `${opts['auth']['basic']['username']}:${
+            opts['auth']['basic']['password'] || ''
+          }`,
+        )}`,
+      )
+    }
+    return ctx.createClient({...opts, headers})
+  },
 } satisfies SdkDefinition<LeverSDKTypes>
 
 export function initLeverSDK(opts: LeverSDKTypes['options']) {
